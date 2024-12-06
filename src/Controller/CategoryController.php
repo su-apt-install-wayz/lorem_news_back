1fe611a7a95404f982745c378ae74e4bc92db5ee
su apt install wayz <wayzzy59@gmail.com>
2024-12-06 19:21:22 +0100

    add crud for category entity

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -11,7 +11,7 @@ use App\Entity\Category;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/api/categories', name: 'api_category', methods: ['get'])]
+    #[Route('/api/categories', name: 'api_categories_all', methods: ['get'])]
     public function index(ManagerRegistry $doctrine): JsonResponse
     {
         $categories = $doctrine->getRepository(Category::class)->findAll();
@@ -27,7 +27,7 @@ class CategoryController extends AbstractController
         return $this->json($data);
     }
 
-    #[Route('/api/category/{id}', name: 'api_category_id', methods: ['get'])]
+    #[Route('/api/categories/{id}', name: 'api_category_id', methods: ['get'])]
     public function show(ManagerRegistry $doctrine, int $id): JsonResponse
     {
         $category = $doctrine->getRepository(Category::class)->find($id);
@@ -42,5 +42,61 @@ class CategoryController extends AbstractController
         ];
 
         return $this->json($data);
+    }
+
+    #[Route('/api/categories', name: 'api_categories_create', methods: ['post'])]
+    public function create(ManagerRegistry $doctrine, Request $request): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+
+        $category = new Category();
+        $category->setName($request->request->get('name'));
+
+        $entityManager->persist($category);
+        $entityManager->flush();
+
+        $data = [
+            'id' => $category->getId(),
+            'name' => $category->getName()
+        ];
+
+        return $this->json($data);
+    }
+
+    #[Route('/api/categories/{id}', name: 'api_categories_update', methods: ['put', 'patch'])]
+    public function update(ManagerRegistry $doctrine, Request $request, int $id): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $category = $doctrine->getRepository(Category::class)->find($id);
+
+        if (!$category) {
+            return $this->json('No products found for id ' . $id, 404);
+        }
+
+        $category->setName($request->request->get('name'));
+        $entityManager->flush();
+
+        $data = [
+            'id' => $category->getId(),
+            'name' => $category->getName()
+        ];
+
+        return $this->json($data);
+    }
+
+    #[Route('/api/categories/{id}', name: 'api_categories_delete', methods: ['delete'])]
+    public function delete(ManagerRegistry $doctrine, int $id): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $category = $doctrine->getRepository(Category::class)->find($id);
+
+        if (!$category) {
+            return $this->json('No products found for id ' . $id, 404);
+        }
+
+        $entityManager->remove($category);
+        $entityManager->flush();
+
+        return $this->json('Deleted category with id ' . $id);
     }
 }
